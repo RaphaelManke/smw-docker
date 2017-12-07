1812cf419a69f675c422e02f27ba7864dd142eb6
Raphael Manke <raphaz@gmx.de>
2017-12-07 14:30:59 +0100

    install extension from json-file

--- semantic-mediawiki-docker/check_db.sh
+++ semantic-mediawiki-docker/check_db.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
+
+# needs 3 Arguments
+# $1 -> search term
+# $2 -> file to search
+# $3 -> readable name what is searched for (for logging)
+function add_to_file {
+    searchTerm=$1
+    file=$2
+    searchName=$3
+    if grep -Rq "$searchTerm" "$file"
+    then
+        echo "$searchName already in $file file. Nothing to do."
+        # code if found
+    else
+        if [ -e "$file" ]
+        then
+            echo "$searchTerm" >> "$file"
+            UPDATE_NEEDED=1
+            echo "added $searchName extension to $file file."
+        else
+            echo "Error $file does not exists. Can not add Extension"
+        fi
+        # code if not found
+    fi
+
+}
+function update_mw {
+    php "maintenance/update.php"
+}
 function main {
     echo "$@"
     BASE="/var/www/html/"
@@ -6,59 +35,94 @@ function main {
     INDICATOR_FILE="LocalSettings.php"
     if [ -f "$INDICATOR_FILE" ]; then
         echo "$INDICATOR_FILE exists. Database already initialized"
-        php "maintenance/update.php"
 
     else
-        echo "$INDICATOR_FILE file does not exist"
-        php maintenance/install.php \
-            --dbname=my_wiki \
-            --dbserver="database" \
-            --installdbuser=wikiuser \
-            --installdbpass=example \
-            --dbuser=wikiuser \
-            --dbpass=example \
-            --server="http://localhost:8080" \
-            --scriptpath="" \
-            --lang=en \
-            --pass=smw-docker \
-            "Example Semantic Media Wiki" \
-            "Admin"
+        INSTALLED="-1"
+        while [ $INSTALLED != 0 ]
+        do
+            echo "$INDICATOR_FILE file does not exist"
+            php maintenance/install.php \
+                --dbname=my_wiki \
+                --dbserver="database" \
+                --installdbuser=wikiuser \
+                --installdbpass=example \
+                --dbuser=wikiuser \
+                --dbpass=example \
+                --server="http://localhost:8080" \
+                --scriptpath="" \
+                --lang=en \
+                --pass=smw-docker \
+                "Example Semantic Media Wiki" \
+                "Admin"
+            INSTALLED=$?
+            echo "Installation finished with code $INSTALLED"
+            sleep 1
+
+        done
     fi
 
 }
-
 function install_smw {
-    echo 'require_once "$IP/extensions/SemanticMediaWiki/SemanticMediaWiki.php";' >> LocalSettings.php
-    echo "enableSemantics( 'example.org' );" >> LocalSettings.php
-    php "maintenance/update.php"
-    #php composer.phar require mediawiki/semantic-media-wiki "~2.5" --update-no-dev
+    echo "starting to install SMW"
+
+    #
+    # check if smw is added to config
+    #
+    add_to_file 'require_once "$IP/extensions/SemanticMediaWiki/SemanticMediaWiki.php";' "$localSettings" "SMW - step 1"
+    add_to_file "enableSemantics( 'example.org' );" "$localSettings" "SMW - step 2"
+
+    echo "finished to install SMW"
+
+
 }
 
 function install_extensions {
-    # TODO Pfad richtig weitergeben
-    sh install_extensions.sh "$dir/install_extensions.json"
+    echo "starting to install Extensions"
+
+    bash "$dir/install_extensions.sh" "$dir/extensions.json"
+
+    echo "finished to install Extensions"
+
 }
 
+function ping_db {
+    echo "try to ping Database"
+    UNREACHEABLE=1
+    UNREACHEABLE_COUNTER=0
+    while [ $UNREACHEABLE -ne "0" ];
+       #do ping -q -c 1 database &> /dev/null; UNREACHEABLE=$?; echo "try again"; sleep 1;
+       do
+        #main ;
+        UNREACHEABLE=$?;
+        echo "database not available or not ready. try again";
+        UNREACHEABLE_COUNTER+=1
+        if [ "$UNREACHEABLE_COUNTER" -gt "20" ]
+        then
+            echo    "more than $UNREACHEABLE_COUNTER attempts to ping the database failed.
+                     Database seems to be not available"
+            exit 2
+        fi
+        sleep 1;
+    done
+    echo "Database is reachable and respond to ping."
+    sleep 3;
 
-dir="${pwd}"
-echo "try to ping Database"
-echo $dir
+}
 
-UNREACHEABLE=1;
-while [ $UNREACHEABLE -ne "0" ];
-   #do ping -q -c 1 database &> /dev/null; UNREACHEABLE=$?; echo "try again"; sleep 1;
-   do
-    #main ;
-    UNREACHEABLE=$?;
-    echo "try again";
-    sleep 1;
-done
-# TODO write script to install/add media_wiki
-#install_smw
-#  TODO write script for installing extensions
+dir="$(pwd)"
+localSettings="LocalSettings.php"
+
+# try ping the database. without connection initialization fails.
+ping_db
+# run main
+main
+# check if SM is activated, if not activate it
+install_smw
+
+#  install or update referenced extensions
 install_extensions
 
-#main
-echo $?
-cd "$dir"
-echo $pwd
+# run update script to make sure everything is up-to-date
+update_mw
+
+
--- semantic-mediawiki-docker/scripts/install_extensions.sh
+++ semantic-mediawiki-docker/scripts/install_extensions.sh
@@ -1,11 +1,95 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# needs 3 Arguments
+# $1 -> search term
+# $2 -> file to search
+# $3 -> readable name what is searched for (for logging)
+
+function add_to_file {
+    searchTerm=$1
+    file=$2
+    searchName=$3
+
+    if grep -Rq "$searchTerm" "$file"
+    then
+        echo "$searchName already in $file file. Nothing to do."
+        # code if found
+    else
+        if [ -e "$file" ]
+        then
+            echo "$searchTerm" >> "$file"
+            echo "added $searchName extension to $file file."
+        else
+            echo "Error $file does not exists. Can not add Extension"
+        fi
+        # code if not found
+    fi
+}
+
+#
+# helper function to check version numbers
+#
+function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
+
+
+function main {
+    extensions=`cat $1`
+    #echo "$extensions"
+    NAME=($(jq -r '.extensions[].name' "$1"))
+    echo "$NAME"
+    #while read NAME
+    for i in "${NAME[@]}"
+    do
+        localName="$i"
+        echo "$localName"
+        extPath="$basePath/$localName/"
+
+        # extensions already exists.
+        if [ -d "$extPath" ]
+        then
+            cd "$extPath"
+            echo $(pwd)
+            git pull
+            if [ $? -eq 0 ]
+            then
+                echo "successfully updated $localName"
+            else
+                echo "Error pulling $localName"
+            fi
+        # extension not found, cloning it.
+        else
+            git clone "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/$localName.git" $basePath/$localName
+            if [ $? -eq 0 ]
+            then
+                echo "successfully cloned $localName"
+            else
+                echo "Error cloning $localName"
+            fi
+
+        fi
+        # go back to working directory
+        cd "$workDir"
+
+        # see https://www.mediawiki.org/wiki/Manual:Extensions
+        if version_gt "$MEDIAWIKI_MAJOR_VERSION" "1.24"
+        then
+            echo "Adding new Syntax"
+            searchTerm="wfLoadExtension( '$localName' );"
+        else
+            echo "Adding old Syntax"
+            searchTerm="require_once \"\$IP/extensions/$localName/$localName.php\";"
+        fi
+        # check if extension is in localSettings file.
+        add_to_file "$searchTerm" "$localSettings" "$localName"
+
+
+    done
+
+}
 
 # Config
-basePath="mediawiki/extensions/"
+workDir=$(pwd)
+basePath="extensions"
+localSettings="LocalSettings.php"
 
-cat $1 | \
-    jq -r '.extensions[].url' | \
-    while read URL
-        do
-            git clone $URL /var/www/html/extensions
-        done
+main $1
