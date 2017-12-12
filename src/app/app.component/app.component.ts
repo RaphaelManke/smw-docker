ffab234d9b27fa03a68e7e4e6e5080a2f3f58b86
Patrick Eisele <paddieiseg@gmail.com>
2017-12-12 01:55:26 +0100

    a lot tested

--- src/app/app.component/app.component.ts
+++ src/app/app.component/app.component.ts
@@ -3,6 +3,10 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {HttpService} from '../http.service';
 import {Extension} from '../extension';
 import * as fileSaver from 'file-saver';
+import {Observable} from 'rxjs/Observable';
+import {HttpClient} from '@angular/common/http';
+import {forEach} from '@angular/router/src/utils/collection';
+import {observable} from 'rxjs/symbol/observable';
 
 @Component({
   selector: 'app-root',
@@ -15,37 +19,72 @@ export class AppComponent implements OnInit, DoCheck {
   title = 'app';
   protected hide = true;
 
-  promiseExtension: Promise<Extension[]>;
-  data: Extension[];
 
+  isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+
   errorString: string;
+
+  url = '//raw.githubusercontent.com/wikimedia/mediawiki-extensions/master/.gitmodules';
+  data = '';
+  content;
+  extensions: Array<Extension> = new Array<Extension>();
+
+  promiseExtensions: Promise<string>;
 
   @Input() username; password;
 
   constructor(private formBuilder: FormBuilder,
-              private httpService: HttpService) {}
+              private http: HttpClient) {
+  }
 
-  async ngOnInit() {
+  ngOnInit() {
     this.firstFormGroup = this.formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
     this.secondFormGroup = this.formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
-    this.promiseExtension = this.httpService.getExtensions();
-    this.promiseExtension.then(
-      extensions => this.data = extensions,
-      error =>  this.errorString = <any>error);
+    this.thirdFormGroup = this.formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+
+     this.promiseExtensions = this.getAllExtension();
+     this.promiseExtensions.then(
+       
+     )
+
+    console.log(this.data);
+
   }
 
-  ngDoCheck() {
-    // console.log(this.data);
+  async getAllExtension(): Promise<string> {
+    try {
+      return await this.http.get(this.url, {responseType: 'text'}).toPromise();
+    } catch (error) {
+      console.log('something went wrong');
+      return null;
+    }
   }
+
+  createTable(data): void {
+    this.content = data.trim().split('[', 3);
+    for (let i = 0; i < this.content.length; i++) {
+      const name_start = this.content[i].indexOf('= ') + 2;
+      const name_end = this.content[i].indexOf('url', name_start + 1) - 1;
+      const url_start = this.content[i].indexOf('= ', name_end) + 2;
+      const url_end = this.content[i].indexOf('branch', url_start + 1) - 1;
+      this.extensions [i].name = this.content[i].substring(name_start, name_end);
+      this.extensions [i].url = this.content[i].substring(url_start, url_end);
+    }
+  }
+
+  ngDoCheck() {  }
 
   saveFile(): void {
-    let file = new File(["Hello, world!"], "stack.txt", {type: "text/plain;charset=utf-8"});
+    let file = new File(["Hello, world!"], "stack.json", {type: "text/plain;charset=utf-8"});
     fileSaver.saveAs(file);
   }
 
